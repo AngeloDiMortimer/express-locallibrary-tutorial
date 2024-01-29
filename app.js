8ca9fb4514f0c7b438b6ee44a73a0c6692ba766f
AngeloDiMortimer <70825802+AngeloDiMortimer@users.noreply.github.com>
2024-01-29 15:51:41 -0400

    removed limiter

--- app.js
+++ app.js
@@ -15,13 +15,6 @@ const { dbConnection } = require('./database');
 
 var app = express();
 
-// Set up rate limiter: maximum of twenty requests per minute
-const RateLimit = require("express-rate-limit");
-const limiter = RateLimit({
-  windowMs: 1 * 60 * 1000, // 1 minute
-  max: 20,
-});
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
